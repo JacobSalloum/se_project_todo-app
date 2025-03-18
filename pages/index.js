4b4dc1d1d23ca44243c69c22155d64bb763bbf97
JacobSalloum <jacobgsalloum@gmail.com>
2025-03-18 12:47:58 -0400

    add functionality to handleEscapeClose and add overlay click close along with code for getInputValues

--- pages/index.js
+++ pages/index.js
@@ -11,20 +11,23 @@ const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopupEl = document.querySelector("#add-todo-popup");
 const addTodoForm = document.forms["add-todo-form"];
 const addTodoCloseBtn = addTodoPopupEl.querySelector(".popup__close");
-//const todoTemplate = document.querySelector("#todo-template");
 export const todosList = document.querySelector(".todos__list");
-
-// const openModal = (modal) => {
-//   modal.classList.add("popup_visible");
-// };
-
-// const closeModal = (modal) => {
-//  modal.classList.remove("popup_visible");
-// };
 
 const addTodoPopup = new PopupWithForm({
   popupSelector: "#add-todo-popup",
-  handleFormSubmit: () => {},
+  handleFormSubmit: (formData) => {
+    const name = formData.name;
+    const dateInput = formData.date;
+
+    const date = new Date(dateInput);
+    date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+
+    const id = uuidv4();
+    const values = { name, date, id };
+    const todo = generateTodo(values);
+    section.addItem(todo);
+    addTodoPopup.close();
+  },
 });
 
 addTodoPopup.setEventListeners();
@@ -39,42 +42,11 @@ addTodoButton.addEventListener("click", () => {
   addTodoPopup.open();
 });
 
-// addTodoCloseBtn.addEventListener("click", () => {
-//   addTodoPopup.close();
-// });
-
-addTodoForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  const name = evt.target.name.value;
-  const dateInput = evt.target.date.value;
-
-  // Create a date object and adjust for timezone
-  const date = new Date(dateInput);
-  date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
-
-  const id = uuidv4();
-  const values = { name, date, id };
-  const todo = generateTodo(values);
-  section.addItem(todo);
-  addTodoPopup.close();
-});
-
-// initialTodos.forEach((item) => {
-// const renderTodo = (item) => {
-// const todo = generateTodo(item);
-// todosList.append(todo);
-// };
-// renderTodo(item);
-// });
-
 const section = new Section({
   items: initialTodos,
   renderer: (item) => {
-    // generate todo item
     const todo = generateTodo(item);
-    // add it to the todo list
-    section.addItem(todo); // use additem() method instead
-    // refer to the foreach loop in this file
+    section.addItem(todo);
   },
   containerSelector: ".todos__list",
 });
